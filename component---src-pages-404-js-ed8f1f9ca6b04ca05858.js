ab912e41503eeaf69d8066925f3ec81128d7b462
sennett <anthony.sennett@gmail.com>
2022-07-01 21:30:30 +0100

    Updates

---
+++ component---src-pages-404-js-ed8f1f9ca6b04ca05858.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_lander=self.webpackChunkgatsby_starter_lander||[]).push([[883],{9616:function(t,e,n){n.r(e);n(7294);var u=n(2813),a=n(3431);e.default=function(){return(0,a.tZ)(u.Z,null,(0,a.tZ)("h1",null,"Not Found"),(0,a.tZ)("p",null,"You just hit a page that doesn't exist..."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-ed8f1f9ca6b04ca05858.js.map
