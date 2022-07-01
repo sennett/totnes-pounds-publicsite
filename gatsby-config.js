de8db187fd3c61fe0f26783dd8bb064acabba22e
sennett <anthony.sennett@gmail.com>
2022-07-01 22:10:53 +0100

    gh pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
 
 module.exports = {
+  pathPrefix: "/totnes-pounds-publicsite",
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-emotion`,
